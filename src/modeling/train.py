01745d8e961ec6cb14d5b6debda80ad4d2557a05
Precious Ratlhagane <precious_555@live.com>
2025-04-17 16:44:45 +0200

    Scale my numerical features for the modelling section

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -1,11 +1,14 @@
-from features import preprocess_features
 import os
 import sys
 
+import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import mean_absolute_error, r2_score, root_mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+from features import preprocess_features
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -23,14 +26,20 @@ y = df_processed['Premium_Amount']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
+
+# Scale the features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
 # Train the model
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train_scaled, y_train)
 
 # Predict using the test_set and evaluate
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test_scaled)
 
 
 print("Mean Absoute Error:", mean_absolute_error(y_test, y_pred))
-print("Root Mean Squared Error:", root_mean_squared_error(y_test, y_pred))
+print("Root Mean Squared Error:", np.sqrt(mean_squared_error(y_test, y_pred)))
 print("R-squared:", r2_score(y_test, y_pred))
