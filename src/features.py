f36576f6ecc96e65071d9ee28070760ff495dc35
Precious Ratlhagane <precious_555@live.com>
2025-05-27 16:08:10 +0200

    Perform feature engineering by adding calculated columns

--- src/features.py
+++ src/features.py
@@ -1,11 +1,19 @@
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
+import numpy as np
 
 
 def preprocess_features(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
+
+    # Calculate the proportion of the claims to the number of accidents
+    df['Accident_Claim_Rate'] = df['Number_of_Claims'] / \
+        df['Number_of_Accidents'].replace(0, np.nan)
+    df['Accident_Claim_Rate'].fillna(0, inplace=True)
+
+    # Calculate the claims made per year by each driver
+    df['Claims_per_Year'] = df['Number_of_Claims'] / \
+        df['Years_Driving'].replace(0, np.nan)
+    df['Claims_per_Year'].fillna(0, inplace=True)
 
     # Drop only non-useful columns
     df.drop(columns=["Customer_ID"], inplace=True)
