750349022afb2b3fe9eb93cd2712619dd6919894
Precious Ratlhagane <precious_555@live.com>
2025-06-01 14:21:12 +0200

    Define the input paramters for use later when using the model to generate outputs

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,5 +1,84 @@
 import streamlit as st
 
-car_model_option = st.selectbox("Pick your car model:", [
-                                "BMW", "Volkswagen", "Toyota", "Kia", "Haval"])
-st.write(f"You selected: {car_model_option}")
+# Enter your age
+age = st.number_input(
+    "Enter your age", value=None, placeholder="Type a number..."
+)
+
+st.write("The current age is", age)
+
+# Enter your gender
+gender = st.selectbox("Gender", ["Male", "Female"])
+st.write("You selected:", gender)
+
+# Province
+province = st.selectbox("Free State", "Limpopp", "Gauteng", "Mpumalanga",
+                        "Nothern Cape", "KwaZulu-Natal", "Western Cape", "North West", "Eastern Cape")
+st.write("You selected:", province)
+
+# Employment status
+employment_status = st.selectbox(
+    "Self-employed", "Employed", "Retired", "Unemployed", "Student")
+
+# Years_driving
+years_driving = st.slider(
+    "How many years of driving experience do you have?", 0, 130, 25)
+st.write("You chose:", years_driving)
+
+# Car Make
+car_make = st.selectbox("Ford", "BMW", "Hyundai",
+                        "Toyota", "Mercedes", "Volkswagen")
+st.write("You chose:", car_make)
+
+# Car_Model
+car_model = st.selectbox("Fiesta", "X5", "Tucson", "118i", "Creta", "EcoSport", "i20", "Hilux",
+                         "Corolla", "Ranger", "320i", "Fortuner", "Golf", "A200", "GLA", "Tiguan", "C200", "Polo")
+
+# Marital_Status
+marital_status = st.selectbox("Widowed", "Single", "Married", "Divorced")
+st.write("You chose:", marital_status)
+
+# Vehicle_usage
+vehicle_usage = st.selectbox("Commercial", "Business", "Private")
+st.write("You chose:", vehicle_usage)
+
+# Manufacture_year
+manufacture_year = st.number_input(
+    "Car Manufacture Year:", value=None, placeholder="Type a number...")
+st.write("You chose:", manufacture_year)
+
+# Annual_car_mileage
+annual_car_mileage = st.number_input(
+    "Annual Car Mileage:", st.write("You chose:", car_make))
+st.write("You chose:", annual_car_mileage)
+
+# Number of accidents
+number_of_accidents = st.number_input(
+    "Number of Accidents:", value=None, placeholder="Type a number...")
+st.write("You chose:", number_of_accidents)
+
+# Number of claims
+number_of_claims = st.number_input(
+    "Number of Claims:", value=None, placeholder="Type a number...")
+st.write("You chose:", number_of_claims)
+
+# Car_value
+car_value = st.number_input("Car Value in Rands:",
+                            value=None, placeholder="Type a number...")
+st.write("You chose:", car_value)
+
+# Tracking_device
+tracking_device = st.radio(
+    "Do you have a tracking device installed?",
+    ["Yes", "No"]
+)
+st.write("You chose:", tracking_device)
+
+# Policy_term
+policy_term = st.selectbox("6", "12")
+st.write("You chose:", policy_term)
+
+# Credit Score
+credit_score = st.number_input(
+    "Credit Score", value=None, placeholder="Enter your credit score...")
+st.write("You chose:", credit_score)
