1cc7c7f16aa2940fb400bcfad26f16335b4d0beb
Precious Ratlhagane <precious_555@live.com>
2025-06-06 15:52:30 +0200

    Populate the about page

---
+++ streamlit/pages/about.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.set_page_config(page_title="About", page_icon="ℹ️")
+
+st.title("ℹ️ About SafeDrive Analytics")
+
+st.markdown("""
+SafeDrive Analytics is a fictional South African insurance startup aiming to transform premium pricing using modern machine learning techniques. 
+
+### 🎯 Our Mission
+To improve fairness and accuracy in car insurance pricing by leveraging data science techniques.
+
+### 👩‍💻 Built By
+This prototype was developed by a data science intern as part of a predictive analytics project. 
+
+### 🔗 Project Repository
+[Github Repo](https://github.com/preciousratlhagane/car-insurance-predictor-using-ml.git)
+
+### 🚧 Limitations & Future 
+- Incoporate more real-world data
+- Expand model to other insurance types
+- Implement user authentication and persistent user history
+
+### 📩 Contact
+Feel free to connect on [LinkedIn]https://www.linkedin.com/in/preciousratlhagane)
+
+---
+
+*This is a prototype application developed for educational and demonstration purposes only.*
+""")
